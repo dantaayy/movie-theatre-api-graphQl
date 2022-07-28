7d287020c3a832f345850a8540d84f9f8e79b750
dantaayy <dantaayy@gmail.com>
2022-07-28 10:53:26 -0700

    modularized the resolver into separate file

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,5 @@
+const Query = require('./resolvers/query')
+
 module.exports = {
-    Query: {
-        shows: (parent, args, {dataSources}, info) => {
-            return dataSources.showAPI.getShows(args);
-        },
-        showsByRating: (parent, {rating}, {dataSources}, info) => {
-            return dataSources.showAPI.getShowsByRating(rating);
-        },
-        users: (parent, args, {dataSources}, info) => {
-            return dataSources.userAPI.getUsers(args);
-        }
-    }
+    Query 
 }
